79cb2539b6bebe85a8c910f5514fea1014334e7e
Kay Freyer <kay.freyer@icloud.com>
2024-08-17 21:21:22 +0200

    working split

--- ft_split.c
+++ ft_split.c
@@ -12,17 +12,16 @@
 
 #include "libft.h"
 
-char	*advance_until_sep(char *str, char c)
+char	*jump_sep(char *str, char sep)
 {
-	while (*str && (*str != c))
+	while (*str && *str == sep)
 		str++;
 	return (str);
 }
 
-char	*advance_after_sep(char *str, char c)
+char	*jump_non_sep(char *str, char sep)
 {
-	str = advance_until_sep(str, c);
-	while (*str && (*str == c))
+	while (*str && (*str != sep))
 		str++;
 	return (str);
 }
@@ -36,40 +35,37 @@ int	count_words(char *str, char c)
 		return (0);
 	if (*str != c)
 		count++;
-	str = advance_after_sep(str, c);
+	str = jump_non_sep(str, c);
+	str = jump_sep(str, c);
 	while (*str && (*str == c))
 		str++;
 	while (*str)
 	{
 		count++;
-		str = advance_after_sep(str, c);
+		str = jump_non_sep(str, c);
+		str = jump_sep(str, c);
 	}
 	return (count);
 }
 
-void	free_strs(char **strs, size_t len)
+char	*extract_string(char *str, char *end_word, char **strs, size_t count)
 {
 	size_t	idx;
 
-	idx = 0;
-	while (idx < len)
-		free(strs[idx++]);
-	free(strs);
-}
-
-char	*extract_string(char *str, char *end_word)
-{
-	char	*ret;
-	size_t	idx;
-
-	ret = (char *)malloc(((end_word - str) + 1) * sizeof(char));
-	if (ret == NULL)
+	strs[count] = (char *)malloc(((end_word - str) + 1) * sizeof(char));
+	if (strs[count] == NULL)
+	{
+		idx = 0;
+		while (idx < count)
+			free(strs[idx++]);
+		free(strs);
 		return (NULL);
+	}
 	idx = 0;
 	while (str != end_word)
-		ret[idx++] = *str++;
-	ret[idx] = '\0';
-	return (ret);
+		strs[count][idx++] = *str++;
+	strs[count][idx] = '\0';
+	return (strs[count]);
 }
 
 char	**ft_split(char *str, char c)
@@ -77,26 +73,22 @@ char	**ft_split(char *str, char c)
 	char	**strs;
 	char	*end_word;
 	int		nbr_of_words;
-	int		count;
+	size_t	count;
 
 	nbr_of_words = count_words(str, c);
 	strs = (char **)malloc((nbr_of_words + 1) * sizeof(char *));
 	if (strs == NULL)
 		return (NULL);
-	while (*str && *str == c)
-		str++;
+	str = jump_sep(str, c);
 	count = 0;
 	while (nbr_of_words-- > 0)
 	{
-		end_word = advance_until_sep(str, c);
-		strs[count] = extract_string(str, end_word);
-		if (strs[count] == NULL)
-		{
-			free_strs(strs, count);
+		end_word = jump_non_sep(str, c);
+		if (extract_string(str, end_word, strs, count) == NULL)
 			return (NULL);
-		}
+		str = jump_non_sep(str, c);
+		str = jump_sep(str, c);
 		count++;
-		str = advance_after_sep(str, c);
 	}
 	strs[count] = NULL;
 	return (strs);
