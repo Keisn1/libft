21e5f3c622ff4ec1bfc7701a0c0fe21f38e46038
Kay Freyer <kay.freyer@icloud.com>
2024-08-17 17:05:02 +0200

    add test_ft_substr

---
+++ tests/test_ft_substr.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "libft.h"
+
+struct FtSubstrTestParam {
+	const char* s;
+	unsigned int start;
+	size_t len;
+	const char* want;
+};
+
+class FtSubstrTest : public ::testing::TestWithParam<FtSubstrTestParam> {};
+
+TEST_P(FtSubstrTest, HandlesVariousInputs) {
+	FtSubstrTestParam param = GetParam();
+	char* got = ft_substr(param.s, param.start, param.len);
+
+	EXPECT_STREQ(got, param.want);
+	free(got);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+	FtSubstrTests,
+	FtSubstrTest,
+	::testing::Values(
+		FtSubstrTestParam{"hola", 0, 18446744073709551614, "hola"},
+		FtSubstrTestParam{"lorem ipsum dolor sit amet", 7, 10, "psum dolor"}
+		)
+	);
