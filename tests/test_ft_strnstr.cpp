2a072cf68df2ec81c3e32d3500680d06f37c434e
Kay Freyer <kay.freyer@icloud.com>
2024-08-17 18:29:32 +0200

    fix strnstr

--- tests/test_ft_strnstr.cpp
+++ tests/test_ft_strnstr.cpp
@@ -13,7 +13,7 @@ class FtStrnstrTest : public :: testing::TestWithParam<FtStrnstrTestParams>{};
 
 TEST_P(FtStrnstrTest, HandleVariousInputs) {
 	FtStrnstrTestParams params = GetParam();
-	char* got = ft_strnstr(params.big, params.little, params.len);
+	char *got =  ft_strnstr(params.big, params.little, params.len);
 	char* want = strnstr(params.big, params.little, params.len);
 	EXPECT_STREQ(got, want);
 }
@@ -22,9 +22,13 @@ INSTANTIATE_TEST_SUITE_P(
 	FtStrnstrTests,
 	FtStrnstrTest,
 	::testing::Values(
+		FtStrnstrTestParams{"lorem ipsum dolor sit amet", "dolor", 0},
+		FtStrnstrTestParams{"", "", 0},
+		FtStrnstrTestParams{"", "", 20},
 		FtStrnstrTestParams{"OuterInnerString", "Inner", 20},
 		FtStrnstrTestParams{"OuterInnerString", "", 20},
 		FtStrnstrTestParams{"OuterInnerString", "notFound", 20},
 		FtStrnstrTestParams{"", "notFound", 20}
 		)
 	);
+
