191a0d5ac0ff2ae86b8c4b07cf6fba624f2f10f8
Kay Freyer <kay.freyer@icloud.com>
2024-08-21 20:26:51 +0200

    refine ft_memcpy

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	size_t			idx;
 
-	if (!dest || !src)
-		return (dest);
+	if (!dest && !src)
+		return dest;
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	idx = 0;
