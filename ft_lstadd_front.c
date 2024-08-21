3049540787c504c627efdcefaf08d3068881bec8
Kay Freyer <kay.freyer@icloud.com>
2024-08-21 14:37:21 +0200

    norm bonus

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -12,8 +12,11 @@
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new) {
-	t_list *tmp = *lst;
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	tmp = *lst;
 	*lst = new;
 	new->next = tmp;
 }
