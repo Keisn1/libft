95349fd036f4f518f8cebc59fde9ba5fe6f7169a
Kay Freyer <kay.freyer@icloud.com>
2024-08-31 18:25:54 +0200

    addback

--- libft.h
+++ libft.h
@@ -70,5 +70,5 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new_node);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
-
+void				ft_lstadd_back(t_list **lst, t_list *new_node);
 #endif // LIBFT_H
