b5c1077b8b060c3376fd35b593e5977fa7b860f9
Kay Freyer <kay.freyer@icloud.com>
2024-11-20 07:18:10 +0100

    from ft_printf

--- tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "test_libft.hpp"
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    // ::testing::GTEST_FLAG(filter) = "FtLstaddBackTests/FtLstaddBackTest*";
-    // ::testing::GTEST_FLAG(filter) = "FtLstaddFrontTest*";
-
-    return RUN_ALL_TESTS();
-}
